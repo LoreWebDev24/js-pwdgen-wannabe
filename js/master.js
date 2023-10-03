43911d193e1f989cba30efb02a6ad87a49353513
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-03 13:40:56 +0200

    add index.html / js / and pseudocode

---
+++ js/master.js
@@ -0,0 +1,10 @@
+// #js-pwdgen-wannabe
+
+// -Creare le variabili con:
+//     -Nome
+let name = prompt('Ciao Booleaner, come ti chiami?');
+console.log(userName);
+//     -Cognome
+//     -Colore Preferito
+//     -Password Generata
+// -Impostare i prompt con le domande   
---
+++ pseudocodice.md
@@ -0,0 +1,7 @@
+#js-pwdgen-wannabe
+
+-Creare le variabili seguenti con annesso il prompt:
+    -Nome
+    -Cognome
+    -Colore Preferito
+    -Password Generata
